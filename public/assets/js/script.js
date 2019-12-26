b64c952a9af124c68ea4138fb69038267a7e917b
Danijela <danijela@tcom.rs>
2019-12-26 13:59:19 +0100

    Fonts update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,63 +1,63 @@
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+$(document).ready(function () {
+	// Add smooth scrolling to all links
+	$("a").on('click', function (event) {
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+		// Make sure this.hash has a value before overriding default behavior
+		if (this.hash !== "") {
+			// Prevent default anchor click behavior
+			event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+			// Store hash
+			var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
+			// Using jQuery's animate() method to add smooth page scroll
+			// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function () {
+
+				// Add hash (#) to URL when done scrolling (default click behavior)
+				window.location.hash = hash;
+			});
+		} // End if
+	});
 });
 
-(function($,sr){
+(function ($, sr) {
 
-  // debouncing function from John Hann
-  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
-  var debounce = function (func, threshold, execAsap) {
-      var timeout;
+	// debouncing function from John Hann
+	// http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+	var debounce = function (func, threshold, execAsap) {
+		var timeout;
 
-      return function debounced () {
-          var obj = this, args = arguments;
-          function delayed () {
-              if (!execAsap)
-                  func.apply(obj, args);
-              timeout = null;
-          };
+		return function debounced() {
+			var obj = this, args = arguments;
+			function delayed() {
+				if (!execAsap)
+					func.apply(obj, args);
+				timeout = null;
+			};
 
-          if (timeout)
-              clearTimeout(timeout);
-          else if (execAsap)
-              func.apply(obj, args);
+			if (timeout)
+				clearTimeout(timeout);
+			else if (execAsap)
+				func.apply(obj, args);
 
-          timeout = setTimeout(delayed, threshold || 100);
-      };
-  }
-  // smartresize 
-  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+			timeout = setTimeout(delayed, threshold || 100);
+		};
+	}
+	// smartresize 
+	jQuery.fn[sr] = function (fn) { return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
 
-})(jQuery,'smartresize');
-
-
+})(jQuery, 'smartresize');
 
 
 
 
 
-(function(){
+
+
+(function () {
 
 	$wrapper = $('#wrapper');
 	$drawerRight = $('#drawer-right');
@@ -88,30 +88,30 @@ $(document).ready(function(){
 	// SlideNav
 	///////////////////////////////
 
-	function setSlideNav(){
-		jQuery(".toggleDrawer").click(function(e){
+	function setSlideNav() {
+		jQuery(".toggleDrawer").click(function (e) {
 			//alert($wrapper.css('marginRight'));
 			e.preventDefault();
 
-			if($wrapper.css('marginLeft')=='0px'){
-				$drawerRight.animate({marginRight : 0},500).addClass('nav-active');
-				$wrapper.animate({marginLeft : -300},500);
+			if ($wrapper.css('marginLeft') == '0px') {
+				$drawerRight.animate({ marginRight: 0 }, 500).addClass('nav-active');
+				$wrapper.animate({ marginLeft: -300 }, 500);
 			}
-			else{
-				$drawerRight.animate({marginRight : -300},500).removeClass('nav-active');;
-				$wrapper.animate({marginLeft : 0},500);
+			else {
+				$drawerRight.animate({ marginRight: -300 }, 500).removeClass('nav-active');;
+				$wrapper.animate({ marginLeft: 0 }, 500);
 			}
-			
+
 		})
 	}
 
-	function setHeaderBackground() {		
+	function setHeaderBackground() {
 		var scrollTop = jQuery(window).scrollTop(); // our current vertical position from the top	
-		
-		if (scrollTop > 300 || jQuery(window).width() < 700) { 
+
+		if (scrollTop > 300 || jQuery(window).width() < 700) {
 			jQuery('#header .top').addClass('solid');
 		} else {
-			jQuery('#header .top').removeClass('solid');		
+			jQuery('#header .top').removeClass('solid');
 		}
 	}
 
@@ -138,39 +138,39 @@ $(document).ready(function(){
 
 
 	//Set Down Arrow Button
-	jQuery('#scrollToContent').click(function(e){
+	jQuery('#scrollToContent').click(function (e) {
 		e.preventDefault();
-		jQuery.scrollTo("#agencije", 1000, { offset:-(jQuery('#header .top').height()), axis:'y' });
+		jQuery.scrollTo("#galerija", 1000, { offset: -(jQuery('#header .top').height()), axis: 'y' });
 	});
 
-	jQuery('nav > ul > li > a').click(function(e){
+	jQuery('nav > ul > li > a').click(function (e) {
 		e.preventDefault();
-		jQuery.scrollTo(jQuery(this).attr('href'), 400, { offset:-(jQuery('#header .top').height()), axis:'y' });
+		jQuery.scrollTo(jQuery(this).attr('href'), 400, { offset: -(jQuery('#header .top').height()), axis: 'y' });
 	})
 
-	jQuery(window).scroll( function() {
-	   setHeaderBackground();
+	jQuery(window).scroll(function () {
+		setHeaderBackground();
 	});
-	
-	jQuery("[data-scroll]").on("click", function(e){
+
+	jQuery("[data-scroll]").on("click", function (e) {
 		e.preventDefault();
 		var $this = $(this);
-		var $thisScroll = jQuery("#"+$this.data("scroll"));
-		if($($thisScroll)[0]){
-			jQuery.scrollTo(jQuery("#"+$this.data("scroll")), 400, { offset:-(jQuery('#header .top').height()), axis:'y' });
+		var $thisScroll = jQuery("#" + $this.data("scroll"));
+		if ($($thisScroll)[0]) {
+			jQuery.scrollTo(jQuery("#" + $this.data("scroll")), 400, { offset: -(jQuery('#header .top').height()), axis: 'y' });
 		}
 	});
-	
-	jQuery("[data-email]").each(function(){
+
+	jQuery("[data-email]").each(function () {
 		var addr = jQuery(this).data("email");
 		var emaddr = '';
-		for(var i=0; i<addr.length;i++){
+		for (var i = 0; i < addr.length; i++) {
 			emaddr += String.fromCharCode(addr.charCodeAt(i))
 		}
-		jQuery(this).attr('href', 'mailto:'+emaddr).html(emaddr);
+		jQuery(this).attr('href', 'mailto:' + emaddr).html(emaddr);
 	});
-	
-	
+
+
 })();
 
 /*
@@ -196,83 +196,83 @@ span.onclick = function() {
 }
 */
 
-	$("#form_contact").on('submit', function(e){
-		var $f = $(this);
-		e.preventDefault();
-		$.ajax({
-			type: "POST",
-			url: 'contact.php',
-			data: $f.serialize(),
-			dataType: "json",
-			success: function() {},
-			complete: function(e) {
-				var m = $.parseJSON(e.responseText);
-				setTimeout(function() {
-					"success" == m.response ? ($f.find("input").val('').find('textarea').val(''), alert(m.text)) : alert(m.text);
-				}, 750);
-			},
-			error: function() {
-				alert('Greška');
-			}
-		})
-	});
+$("#form_contact").on('submit', function (e) {
+	var $f = $(this);
+	e.preventDefault();
+	$.ajax({
+		type: "POST",
+		url: 'contact.php',
+		data: $f.serialize(),
+		dataType: "json",
+		success: function () { },
+		complete: function (e) {
+			var m = $.parseJSON(e.responseText);
+			setTimeout(function () {
+				"success" == m.response ? ($f.find("input").val('').find('textarea').val(''), alert(m.text)) : alert(m.text);
+			}, 750);
+		},
+		error: function () {
+			alert('Greška');
+		}
+	})
+});
 
 // <!-- Slider na pocetnoj -->
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+	showDivs(slideIndex += n);
 }
 
 function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlide");
-  if (n > x.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
-  }
-  x[slideIndex-1].style.display = "block";  
+	var i;
+	var x = document.getElementsByClassName("mySlide");
+	if (n > x.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = x.length }
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[slideIndex - 1].style.display = "block";
 }
 
 // <!-- Galerija -->
 function openModal() {
-	if($("#drawer-right").hasClass("nav-active")){
+	if ($("#drawer-right").hasClass("nav-active")) {
 		$(".toggleDrawer").trigger("click");
 	}
 	document.getElementById('myModal').style.display = "block";
 }
 
 function closeModal() {
-  document.getElementById('myModal').style.display = "none";
+	document.getElementById('myModal').style.display = "none";
 }
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+	showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("demo");
+	var captionText = document.getElementById("caption");
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+	captionText.innerHTML = dots[slideIndex - 1].alt;
 }
