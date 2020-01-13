c0c64267ab43a21923e6148cca561ffb41d448cf
Danijela <danijela@tcom.rs>
2020-01-13 11:56:11 +0100

    Package json update

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,19 @@ const gulp = require('gulp');
 const cleancss = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
-const uncss = require('gulp-uncss');
+// const uncss = require('gulp-uncss');
 
 // Remove unused CSS
-gulp.task('uncss', async function () {
-    gulp.src('assets/css/style.css')
-        .pipe(uncss({
-            html: ['index.html', '/*.html']
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('dist/css'));
-});
+// gulp.task('uncss', async function () {
+//     gulp.src('assets/css/style.css')
+//         .pipe(uncss({
+//             html: ['index.html', '/*.html']
+//         }))
+//         .pipe(rename({
+//             suffix: '.min'
+//         }))
+//         .pipe(gulp.dest('dist/css'));
+// });
 
 // Prefixer CSS
 gulp.task('autoprefixer', async function () {
